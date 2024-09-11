18bc51530be8b55d7738b48a2ae9a14177a1e84b
nikhil1119 <nikhilgadiwadd123@gmail.com>
2024-09-12 00:11:41 +0530

    first commit

---
+++ src/main/java/com/dreamshops/service/order/IOrderService.java
@@ -0,0 +1,12 @@
+package com.dreamshops.service.order;
+
+import com.dreamshops.dto.OrderDto;
+import com.dreamshops.model.Order;
+
+import java.util.List;
+
+public interface IOrderService {
+    Order placeOrder(Long userId);
+    OrderDto getOrder(Long orderId);
+    List<OrderDto> getUserOrders(Long userId);
+}
---
+++ src/main/java/com/dreamshops/service/user/IUserService.java
@@ -0,0 +1,16 @@
+package com.dreamshops.service.user;
+
+import com.dreamshops.dto.UserDto;
+import com.dreamshops.model.User;
+import com.dreamshops.request.CreateUserRequest;
+import com.dreamshops.request.UserUpdateRequest;
+
+public interface IUserService {
+
+    User getUserById(Long userId);
+    User createUser(CreateUserRequest request);
+    User updateUser(UserUpdateRequest request, Long userId);
+    void deleteUser(Long userId);
+
+    UserDto convertUserToDto(User user);
+}
